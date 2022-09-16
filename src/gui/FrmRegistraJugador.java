52841f4f80f3cba281f35889d72802121fe71cb4
jorgejacinto9701 <jorgejacinto9701@gmail.com>
2022-09-15 20:57:25 -0500

    Ajustes

--- src/gui/FrmRegistraJugador.java
+++ src/gui/FrmRegistraJugador.java
@@ -118,6 +118,7 @@ public class FrmRegistraJugador extends JFrame implements ActionListener
 			obj.setNombre(nom);
 			obj.setApellido(ape);
 			obj.setFechaNacimiento(Conversiones.toFecha(fec));
+			obj.setEstado(1);
 			
 			JugadorModel model = new JugadorModel();
 			int salida = model.insertaJugador(obj);
